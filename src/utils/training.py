7d1f74c0a3fff09473a5d1fa0f758563e2d222c1
Augusto <augustotaglemontes@gmail.com>
2025-05-08 10:04:38 -0400

    added training class

---
+++ src/utils/training.py
@@ -0,0 +1,74 @@
+import os
+import gym
+import jax
+import jax.numpy as jnp
+from omegaconf import OmegaConf
+from tqdm import tqdm
+from utils.dataset import Maze_Dataset
+import wandb
+from typing import Dict
+     
+
+class Trainer:
+    def __init__(self, cfg, training_cfg):
+        self.cfg = cfg
+        self.training_cfg = training_cfg
+
+        if training_cfg.wandb_log:
+            wandb.init(project="SWG_JAX",
+                       name=cfg.wandb.wandb_exp_name,
+                       config=OmegaConf.to_container(cfg, resolve=True))
+            
+        os.makedirs(cfg.savepath, exist_ok=True) # model path
+
+
+    def train(self):
+        dataset, dataset_val = self.load_dataset(self.training_cfg.dataset_filepath)
+
+        model_cls = self.cfg.method.agent._target_ # FLOW learner
+        agent = globals()[model_cls].create(self.cfg.seed, self.cfg.agent)
+
+        agent = self.train_loop(agent,
+                         dataset,
+                         dataset_val)
+        wandb.finish()
+
+
+    def load_dataset(self, filepath): # TODO falta add weights dataset? maybe use same function... 
+
+        dataset = Maze_Dataset(filepath)
+        
+        dataset, dataset_val = dataset.split(0.96) if self.training_cfg.val_dataset else (dataset, None)
+
+        return dataset, dataset_val
+
+    def log_info(self, info: Dict, step: int, prefix: str):
+
+        info_str = " | ".join([f"{prefix}/{k}: {v}" for k, v in info.items()])
+        print(f"{info_str} | (step {step})")
+
+        if self.training_cfg.wandb_log: wandb.log({f"{prefix}/{k}": v for k, v in info.items()}, step=step)
+
+
+    def train_loop(self, agent, dataset, dataset_val):
+        keys = None
+
+        for step in tqdm(range(1,self.cfg.train.steps+1), smoothing=0.1):
+
+            sample = dataset.sample_jax(self.cfg.train.batch_size, keys=keys)
+            agent, info = agent.update(sample) 
+
+
+            if step % self.training_cfg.log_freq == 0:
+                self.log_info(info, step, prefix="train")
+
+                if dataset_val is not None:
+                    val_batch = dataset_val.sample(self.cfg.train.batch_size, keys=keys)
+                    _, val_info= agent.update(val_batch)
+                    self.log_info(val_info, step, prefix="val")
+            
+            if step % self.training_cfg.save_freq == 0 or step==1:
+                agent.save(self.cfg.savepath, step)
+
+        return agent
+    
